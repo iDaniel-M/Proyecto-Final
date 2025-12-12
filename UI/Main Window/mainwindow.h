4000fbeadde3d9cae2a847038c338fc47a3d185c
iDaniel-M <danymarjosu@gmail.com>
2025-12-12 12:37:34 -0500

    Correcion PDF

--- UI/Main Window/mainwindow.h
+++ UI/Main Window/mainwindow.h
@@ -2,9 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QSqlTableModel>
-#include <QSortFilterProxyModel>
-#include <QModelIndex>
 #include "databasemanager.h"
 
 QT_BEGIN_NAMESPACE
@@ -16,31 +13,26 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
-    // Slots de Botones
+    // --- BOTONES (Nombres según tu diseño .ui) ---
     void on_btnAnadir_clicked();
     void on_btnEditar_clicked();
     void on_btnEliminar_clicked();
-    void on_btnReporte_clicked();
+    void on_btnReporte_clicked(); // <--- AQUÍ ESTÁ EL NOMBRE CORRECTO
 
-    // Slots de Interacción
-    void on_lineEditBuscar_textChanged(const QString &texto);
+    // --- FILTROS ---
+    void on_lineEditBuscar_textChanged(const QString &arg1);
+    void on_Filtrar_currentTextChanged(const QString &arg1);
 
-    // *** CORRECCIÓN: Ahora el nombre coincide con tu UI 'Filtrar' ***
-    void on_Filtrar_currentIndexChanged(int index);
-
-    void on_tableView_doubleClicked(const QModelIndex &index);
+    // Función auxiliar
+    void refreshTable();
+    void aplicarFiltros();
 
 private:
-    void setupModel();
-    void handleComponentDialog(int sourceRow = -1);
-
     Ui::MainWindow *ui;
-    QSqlTableModel *componentModel;
-    QSortFilterProxyModel *proxyModel;
 };
 
 #endif // MAINWINDOW_H
